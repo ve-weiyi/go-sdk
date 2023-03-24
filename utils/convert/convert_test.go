0319aa1d5913b6d5a03550d973a02c92fbc13344
waynie.wei <waynie.wei@anker-in.com>
2023-03-24 10:06:10 +0800

    thin

---
+++ utils/convert/convert_test.go
@@ -0,0 +1,39 @@
+package convert
+
+import (
+	"github.com/ve-weiyi/go-sdk/utils/jsonconv"
+	"log"
+	"path"
+	"strings"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	addImportCode := "hello  github.com/ve-weiyi/ve-admin-store/server/api/v1/test"
+	var (
+		importAlias   string
+		importPackage string
+		packageName   string
+	)
+	// 删除多余的空格
+	addImportCode = strings.Join(strings.Fields(addImportCode), " ")
+	// 以空格划分
+	importArr := strings.Split(addImportCode, " ")
+	log.Println("-->", jsonconv.ObjectToJsonIndent(importArr))
+	switch len(importArr) {
+	case 1:
+		importAlias = ""
+		importPackage = importArr[0]
+		packageName = path.Base(importPackage)
+		break
+	case 2:
+		importAlias = importArr[0]
+		importPackage = importArr[1]
+		packageName = importAlias
+		break
+	default:
+		break
+	}
+
+	log.Println(importAlias, importPackage, packageName)
+}
